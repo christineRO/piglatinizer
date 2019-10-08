61de73cfed4a697f50ca644024b2505213b741e6
christineRO <christinerole@icloud.com>
2019-10-08 01:07:39 +0000

    I made it so the code registers seperate words. :)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-$( "document" ).ready(function() {
+$("document").ready(function() {
 
-var pigLatin = "";
+    var vowel = ['a', 'e', 'i', 'o', 'u'];
 
-$("#button").click(function() {
-    pigLatin = $(".input").val();
-    $(".output").text(pigLatin);
+    var pigLatin = "";
 
-});
+    $("#button").click(function() {
+        pigLatin = $(".input").val();
+        var words = pigLatin.split(' ');
+        console.log(words);
+        console.log(pigLatin);
+
+        $(".output").text(pigLatin);
+
+        words.forEach(function(word){
+            var st1 = word.charAt(0);
+            vowel.includes(st1);
+        console.log(st1);
+        console.log(words);
+        });
+
+
+    });
 
 });
